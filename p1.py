cf0f5374950f8e1217dbb823e6501aa4a8359159
Almaraz7 <calmaraz7@student.ccc.edu>
2024-12-10 14:10:58 -0600

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,10 @@
+fname = input('Enter First Name')
+lastn = input('Enter Last Name')
+address = input('Enter Address')
+city = input('Enter City')
+scode = input('Enter state')
+pcode=input('Enter Postel Code')
+fulln = fname + " " +lastn
+print(fulln)
+print(address)
+print(city,scode,pcode)
